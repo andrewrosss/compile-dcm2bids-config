9a713437212c1e6d1a240aee5fa5759991a90106
Andrew Ross <andrew.ross.mail@gmail.com>
2021-10-16 10:56:19 -0400

    Update tests
    Add e2e test to assert yaml config files can be read

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -21,3 +21,22 @@ def test_cli(datadir: Path):
     assert res.returncode == 0
     assert res.stderr == ""
     assert res.stdout == expected.read_text()
+
+
+@pytest.mark.e2e
+def test_cli_with_json_and_yaml_config(datadir: Path):
+    config1 = datadir / "config1.json"
+    config3 = datadir / "config3.yaml"
+    expected = datadir / "merged_config1_config3.json"
+
+    res = subprocess.run(
+        ("compile-dcm2bids-config", config1, config3),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+        encoding="utf8",
+    )
+
+    assert res.returncode == 0
+    assert res.stderr == ""
+    assert res.stdout == expected.read_text()
